080a9cf3edaeb2be41f620066f90ea6cebd3c395
mediator007 <mediator007@mail.ru>
2022-05-04 11:45:49 +0300

    env add

---
+++ db_manage.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+def delete_from_logins():
+    conn = sqlite3.connect("database.db") 
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM logins")
+    print("logins are Empty")
+    conn.commit()
+    conn.close()
+
+
+def select_all_logins():
+    conn = sqlite3.connect("database.db") 
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM logins")
+    result = cursor.fetchall()
+    print(result)
+
+if __name__ == "__main__":
+    # delete_from_logins()
+    select_all_logins()
